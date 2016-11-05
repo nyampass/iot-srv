ec024852ce7d0d4174289de9e764f09f2f80bace
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-11-05 17:36:26 +0900

    Prevent to crash by redis disconnection

--- config/redis.js
+++ config/redis.js
@@ -6,4 +6,11 @@ const bluebird = require('bluebird')
 bluebird.promisifyAll(redis.RedisClient.prototype)
 bluebird.promisifyAll(redis.Multi.prototype)
 
-module.exports = redis.createClient(process.env.REDIS_URL)
+const client = redis.createClient(process.env.REDIS_URL)
+
+client.on('error', err => {
+  // prevent to crash
+  console.log(err.stack)
+})
+
+module.exports = client
